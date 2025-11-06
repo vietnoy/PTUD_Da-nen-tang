93e68a39a7e013edabae4ec1989ce28454bce4ee
Hieu1607 <nguyenhieu16072004@gmail.com>
2025-11-06 17:11:38 +0700

    Add some init files

--- backend/app/api/__init__.py
+++ backend/app/api/__init__.py
@@ -1,10 +1,14 @@
 """API package."""
+
 from fastapi import APIRouter
+
 from .auth import router as auth_router
+from .unit import router as unit_router
 from .users import router as users_router
 
 api_router = APIRouter()
 api_router.include_router(auth_router)
 api_router.include_router(users_router)
+api_router.include_router(unit_router)
 
 __all__ = ["api_router"]
